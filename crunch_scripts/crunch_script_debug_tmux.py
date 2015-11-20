f9e4e7a25575e85bfa6a4d3f4cde493517e0bb76
Joshua Randall <joshua.randall@sanger.ac.uk>
2015-11-20 16:34:12 +0000

    adds crunch_script_debug_tmux.py

---
+++ crunch_scripts/crunch_script_debug_tmux.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import pickle
+import time
+import sys
+
+container = os.environ.get("HOSTNAME", "<container_id>")
+node = os.environ.get("TASK_SLOT_NODE", "<task_slot_node>")
+print "*******************************************************************************\nStarting debug session, try: `ssh %s docker exec -it %s tmux attach` to enter the container and run your real script interactively\n*******************************************************************************" % (node, container)
+
+try:
+  ( child_pid, fd ) = pty.fork()
+except OSError as e:
+  print "ERROR forking a pty: " + str(e)
+
+if child_pid == 0:
+  # in child
+  sys.stdout.flush()
+  try:
+      os.execl("/usr/bin/tmux","/usr/bin/tmux")
+  except:
+    print "ERROR cannot spawn tmux!"
+    raise
+else:
+  # in parent
+  try:
+    tmux = os.fdopen(fd)
+  except:
+    print "ERROR cannot open fd from tmux"
+    raise
+  while True:
+    print "TMUX: " + tmux.readline()
