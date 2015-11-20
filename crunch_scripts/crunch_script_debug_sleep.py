7c9f95f35a8f9e8149175d29363f507ec1b8ece1
Joshua Randall <joshua.randall@sanger.ac.uk>
2015-11-20 16:20:32 +0000

    adds full path to tmux and matching arg0

--- crunch_scripts/crunch_script_debug_sleep.py
+++ crunch_scripts/crunch_script_debug_sleep.py
@@ -19,7 +19,7 @@ node = os.environ.get("TASK_SLOT_NODE", "<task_slot_node>")
 print "*******************************************************************************\nStarting debug session, try: `ssh %s docker exec -it %s tmux attach` to enter the container and run your real script interactively\n*******************************************************************************" % (node, container)
 
 sys.stdout.flush()
-os.execlp("tmux")
+os.execl("/usr/bin/tmux","/usr/bin/tmux")
 
 #print "Feeling very sleepy, give me a kill when you want me to wake up and die!"
 #while(1):
