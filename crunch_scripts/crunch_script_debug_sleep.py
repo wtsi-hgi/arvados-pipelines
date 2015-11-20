056829138fb6649004fb8d2b2561c809871490f9
Joshua Randall <joshua.randall@sanger.ac.uk>
2015-11-20 17:32:47 +0000

    changes env printing to be bash sourceable

--- crunch_scripts/crunch_script_debug_sleep.py
+++ crunch_scripts/crunch_script_debug_sleep.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 
 import os
-import pickle
 import time
 import sys
 
 # Dumping environment
-#env = os.environ
-#print "Environment:" + str(env)
-#try:
-#  pickle.dump(env, open("/tmp/crunch_script_debug.env", "w"))
-#except:
-#  raise
-#print "Also dumped environment to /tmp/crunch_script_debug.env"
+env = os.environ
+print "Environment:" + str(env)
+env_f = open("/tmp/crunch_script_debug.env", "w")
+for var in os.environ.keys():
+    print >>enf_f, "export %s=\"%s\"" % (var, os.environ[var])
+close(env_f)
 
 container = os.environ.get("HOSTNAME", "<container_id>")
 node = os.environ.get("TASK_SLOT_NODE", "<task_slot_node>")
-print "*******************************************************************************\nStarting debug session, try: `ssh %s docker exec -it %s tmux attach` to enter the container and run your real script interactively\n*******************************************************************************" % (node, container)
+print "*******************************************************************************\nStarting debug session, try: `ssh -t %s docker exec -it %s bash` to enter the container and run your real script interactively\n*******************************************************************************" % (node, container)
 
-sys.stdout.flush()
-os.execl("/usr/bin/tmux","/usr/bin/tmux")
-
-#print "Feeling very sleepy, give me a kill when you want me to wake up and die!"
-#while(1):
-#    time.sleep(60)
+print "Feeling very sleepy, give me a kill when you want me to wake up and die!"
+while(1):
+    time.sleep(60)
