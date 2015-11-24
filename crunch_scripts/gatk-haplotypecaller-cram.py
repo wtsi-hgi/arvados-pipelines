e5f57faa537595c98762959864959bff3fed4183
Joshua Randall <joshua.randall@sanger.ac.uk>
2015-11-24 01:55:50 +0000

    adds option to mount instead of copy input data

--- crunch_scripts/gatk-haplotypecaller-cram.py
+++ crunch_scripts/gatk-haplotypecaller-cram.py
@@ -4,13 +4,16 @@ import os           # Import the os module for basic path manipulation
 import arvados      # Import the Arvados sdk module
 import re
 
+copy_ref = False
+copy_input = False
+
 class InvalidArgumentError(Exception):
     pass
 
 class FileAccessError(Exception):
     pass
 
-def one_task_per_cram_file(if_sequence=0, and_end_task=True):
+def one_task_per_cram_file(if_sequence=0, and_end_task=True, ref_input):
     """
     Queue one task for each cram file in this job's input collection.
     Each new task will have an "input" parameter: a manifest
@@ -23,6 +26,40 @@ def one_task_per_cram_file(if_sequence=0, and_end_task=True):
     """
     if if_sequence != arvados.current_task()['sequence']:
         return
+
+    # Ensure we have a .fa reference file with corresponding .fai index and .dict
+    # see: http://gatkforums.broadinstitute.org/discussion/1601/how-can-i-prepare-a-fasta-file-to-use-as-reference
+    reference_coll = arvados.current_job()['script_parameters']['reference_collection']
+    rcr = arvados.CollectionReader(reference_coll)
+    ref_fasta = {}
+    ref_fai = {}
+    ref_dict = {}
+    ref_input = None
+    for rs in rcr.all_streams():
+        for rf in rcr.all_files():
+            if re.search(r'\.fa$', rf.name()):
+                ref_fasta[rs.name(), rf.name()] = rf
+            elif re.search(r'\.fai$', rf.name()):
+                ref_fai[rs.name(), rf.name()] = rf
+            elif re.search(r'\.dict$', rf.name()):
+                ref_dict[rs.name(), rf.name()] = rf
+    for ((s_name, f_name), fasta_f) in ref_fasta.items():
+        fai_f = ref_fai.get((s_name, re.sub(r'fa$', 'fai', f_name)), 
+                            ref_fai.get((s_name, re.sub(r'fa$', 'fa.fai', f_name)), 
+                                        None))
+        dict_f = ref_dict.get((s_name, re.sub(r'fa$', 'dict', f_name)), 
+                              ref_dict.get((s_name, re.sub(r'fa$', 'fa.dict', f_name)), 
+                                           None))
+        if fasta_f and fai_f and dict_f:
+            # found a set of all three! 
+            ref_input = fasta_f.as_manifest()
+            ref_input += fai_f.as_manifest()
+            ref_input += dict_f.as_manifest()
+            break
+    if ref_input is None:
+        raise InvalidArgumentError("Expected a reference fasta with fai and dict in reference_collection. Found [%s]" % ' '.join(rf.name() for rf in rs.all_files()))
+
+    # prepare CRAM input collections
     job_input = arvados.current_job()['script_parameters']['inputs_collection']
     cr = arvados.CollectionReader(job_input)
     cram = {}
@@ -43,56 +80,30 @@ def one_task_per_cram_file(if_sequence=0, and_end_task=True):
         else:
             # no CRAI for CRAM
             raise InvalidArgumentError("No correponding CRAI file found for CRAM file %s" % f_name)
+        # Create task for each CRAM
+        print "Creating new task to process %s" % f_name
         new_task_attrs = {
             'job_uuid': arvados.current_job()['uuid'],
             'created_by_job_task_uuid': arvados.current_task()['uuid'],
             'sequence': if_sequence + 1,
             'parameters': {
-                'input': task_input
+                'input': task_input,
+                'ref': ref_input
                 }
             }
         arvados.api().job_tasks().create(body=new_task_attrs).execute()
     if and_end_task:
+        print "Ending task 0 successfully"
         arvados.api().job_tasks().update(uuid=arvados.current_task()['uuid'],
                                          body={'success':True}
                                          ).execute()
         exit(0)
 
 
-# Ensure we have a .fa reference file with corresponding .fai index and .dict
-# see: http://gatkforums.broadinstitute.org/discussion/1601/how-can-i-prepare-a-fasta-file-to-use-as-reference
-reference_coll = arvados.current_job()['script_parameters']['reference_collection']
-rcr = arvados.CollectionReader(reference_coll)
-ref_fasta = {}
-ref_fai = {}
-ref_dict = {}
-ref_input = None
-for rs in rcr.all_streams():
-    for rf in rs.all_files():
-        if re.search(r'\.fa$', rf.name()):
-            ref_fasta[rs.name(), rf.name()] = rf
-        elif re.search(r'\.fai$', rf.name()):
-            ref_fai[rs.name(), rf.name()] = rf
-        elif re.search(r'\.dict$', rf.name()):
-            ref_dict[rs.name(), rf.name()] = rf
-for ((s_name, f_name), fasta_f) in ref_fasta.items():
-    fai_f = ref_fai.get((s_name, re.sub(r'fa$', 'fai', f_name)), 
-                        ref_fai.get((s_name, re.sub(r'fa$', 'fa.fai', f_name)), 
-                                    None))
-    dict_f = ref_dict.get((s_name, re.sub(r'fa$', 'dict', f_name)), 
-                          ref_dict.get((s_name, re.sub(r'fa$', 'fa.dict', f_name)), 
-                                       None))
-    if fasta_f and fai_f and dict_f:
-        # found a set of all three! 
-        ref_input = fasta_f.as_manifest()
-        ref_input += fai_f.as_manifest()
-        ref_input += dict_f.as_manifest()
-        break
-if ref_input is None:
-    raise InvalidArgumentError("Expected a reference fasta with fai and dict in reference_collection. Found [%s]" % ' '.join(rf.name() for rf in rs.all_files()))
+this_job = arvados.current_job()
 
 # Setup sub tasks 1-N (and terminate if this is task 0)
-one_task_per_cram_file(if_sequence=0, and_end_task=True)
+one_task_per_cram_file(if_sequence=0, and_end_task=True, ref_input)
 
 # Get object representing the current task
 this_task = arvados.current_task()
@@ -100,23 +111,24 @@ this_task = arvados.current_task()
 # We will never reach this point if we are in the 0th task
 assert(this_task['sequence'] != 0)
 
-# Get tmpdir
-tmpdir = this_task.tmpdir
-print "Using tmpdir %s" % tmpdir
-
 # Get reference FASTA
-print "Getting reference FASTA from keep"
-ref_file = None
-tmp_ref = os.path.join(tmpdir, 'ref')
-try:
-    ref_dir = arvados.util.collection_extract(collection = ref_input,
-                                              path = tmp_ref)
-    for f in arvados.util.listdir_recursive(ref_dir):
-        if re.search(r'\.fa$', f):
-            ref_file = os.path.join(ref_dir, f)
-except:
-    print "ERROR getting reference data from keep collection = [%s] into path = [%s]" % (ref_input, tmp_ref)
-    raise
+if copy_ref:
+    print "Getting reference FASTA from keep"
+    ref_file = None
+    tmp_ref = os.path.join(this_job.tmpdir, 'ref')
+    try:
+        ref_dir = arvados.util.collection_extract(collection = ref_input,
+                                                  path = tmp_ref)
+    except:
+        print "ERROR getting reference data from keep collection = [%s] into path = [%s]" % (ref_input, tmp_ref)
+        raise
+else:
+    print "Mounting reference FASTA collection"
+    ref_dir = arvados.get_job_param_mount('ref')
+
+for f in arvados.util.listdir_recursive(ref_dir):
+    if re.search(r'\.fa$', f):
+        ref_file = os.path.join(ref_dir, f)
 if ref_file is None:
     raise InvalidArgumentError("No reference fasta found in reference collection.")
 
@@ -126,20 +138,26 @@ if not os.access(ref_file, os.R_OK):
 # TODO: could check readability of .fai and .dict as well?
 
 # Get single CRAM file for this task 
-task_input = this_task['parameters']['input']
-tmp_input = os.path.join(tmpdir, 'input')
-print "Getting input data from keep for task_input [%s]" % task_input
-try:
-    input_dir = arvados.util.collection_extract(collection = task_input, 
-                                                path = tmp_input)
-    input_cram_files = []
-    for f in arvados.util.listdir_recursive(input_dir):
-        if re.search(r'\.cram$', f):
-            stream_name, input_file_name = os.path.split(f)
-            input_cram_files += [os.path.join(input_dir, f)]
-except:
-    print "ERROR getting input data from keep collection = [%s] into path = [%s]" % (task_input, tmp_input)
-    raise
+input_dir = None
+if copy_input:
+    task_input = this_task['parameters']['input']
+    tmp_input = os.path.join(this_task.tmpdir, 'input')
+    print "Getting input data from keep for task_input [%s]" % task_input
+    try:
+        input_dir = arvados.util.collection_extract(collection = task_input, 
+                                                    path = tmp_input)
+    except:
+        print "ERROR getting input data from keep collection = [%s] into path = [%s]" % (task_input, tmp_input)
+        raise
+else:
+    print "Mounting task input collection"
+    input_dir = arvados.get_job_param_mount('input')
+    
+input_cram_files = []
+for f in arvados.util.listdir_recursive(input_dir):
+    if re.search(r'\.cram$', f):
+        stream_name, input_file_name = os.path.split(f)
+        input_cram_files += [os.path.join(input_dir, f)]
 if len(input_cram_files) != 1:
     raise InvalidArgumentError("Expected exactly one cram file per task.")
 
