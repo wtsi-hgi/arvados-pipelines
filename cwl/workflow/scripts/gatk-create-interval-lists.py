25bd48745797b9e17288d47dbfca781edaa88c0d
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-08-15 11:41:30 +0100

    Improved gatk-create-interval_lists.py

--- cwl/workflow/scripts/gatk-create-interval-lists.py
+++ cwl/workflow/scripts/gatk-create-interval-lists.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python
 
-import os           # Import the os module for basic path manipulation
+import os
 import re
-import subprocess
-import sys
 import argparse
 
-# the amount to weight each sequence contig
+# The amount to weight each sequence contig
 weight_seq = 120000
 
 class InvalidArgumentError(Exception):
     pass
 
-class FileAccessError(Exception):
-    pass
-
 class APIError(Exception):
     pass
 
+def dict_to_interval_list(dict_text):
+    # Load the dict data
 
-def create_interval_lists(reference_coll, skip_sq_sn_r):
-
-    #load the dict data
-    dict_reader = reference_coll
-    
     interval_header = ""
-    dict_lines = dict_reader.readlines()
+    dict_lines = dict_text.split("\n")
     dict_header = dict_lines.pop(0)
     if re.search(r'^@HD', dict_header) is None:
         raise InvalidArgumentError("Dict file in reference collection does not have correct header: [%s]" % dict_header)
     interval_header += dict_header
-    print "Dict header is %s" % dict_header
+    print("Dict header is %s" % dict_header)
     sn_intervals = dict()
     sns = []
     total_len = 0
-    for sq in dict_lines:
-        if re.search(r'^@SQ', sq) is None:
-            raise InvalidArgumentError("Dict file contains malformed SQ line: [%s]" % sq)
-        interval_header += sq
+    for line in dict_lines:
+        if re.search(r'^@SQ', line) is None:
+            raise InvalidArgumentError("Dict file contains malformed SQ line: [%s]" % line)
+        interval_header += line
+
+        # Locate the SN and LN parameters
         sn = None
         ln = None
-        for tagval in sq.split("\t"):
+        for tagval in line.split("\t"):
             tv = tagval.split(":", 1)
             if tv[0] == "SN":
                 sn = tv[1]
@@ -49,39 +43,28 @@ def create_interval_lists(reference_coll, skip_sq_sn_r):
             if sn and ln:
                 break
         if not (sn and ln):
-            raise InvalidArgumentError("Dict file SQ entry missing required SN and/or LN parameters: [%s]" % sq)
-        assert(sn and ln)
+            raise InvalidArgumentError("Dict file SQ entry missing required SN and/or LN parameters: [%s]" % line)
+
         if sn_intervals.has_key(sn):
-            raise InvalidArgumentError("Dict file has duplicate SQ entry for SN %s: [%s]" % (sn, sq))
-        if skip_sq_sn_r.search(sn):
-            next
+            raise InvalidArgumentError("Dict file has duplicate SQ entry for SN %s: [%s]" % (sn, line))
+
         sn_intervals[sn] = (1, int(ln))
         sns.append(sn)
         total_len += int(ln)
     total_sequences = len(sns)
 
     # Chunk the genome into genome_chunks equally sized pieces and create intervals files
-    print "Total sequences included: %s" % (total_sequences)
-    print "Total genome length is %s" % total_len
+    print("Total sequences included: %s" % (total_sequences))
+    print("Total genome length is %s" % total_len)
     total_points = total_len + (total_sequences * weight_seq)
-    print "Total points to split: %s" % (total_points)
-    # chunk_points = int(total_points / genome_chunks)
-
-    basename = os.path.splitext(os.path.basename(dict_reader.name))[0]+'.interval_list'
-    # directory = os.path.join(os.getcwd(), basename)
-    # os.makedirs(directory)
-    # os.chdir(directory)
-
-    inpt_name = os.path.basename(dict_reader.name)
-    f = open(basename, 'w+')
-
-    f.write(interval_header)
-
+    print("Total points to split: %s" % (total_points))
     
+    interval_list_text = interval_header
+
     remaining_points = total_points
     chunk_num = 1
-    while len(sns) > 0:
-        chunk_num+=1
+    while sns:
+        chunk_num += 1
         sn = sns.pop(0)
         remaining_points -= weight_seq
         if remaining_points <= 0:
@@ -90,57 +73,39 @@ def create_interval_lists(reference_coll, skip_sq_sn_r):
         if not sn_intervals.has_key(sn):
             raise ValueError("sn_intervals missing entry for sn [%s]" % sn)
         start, end = sn_intervals[sn]
-        if (end-start+1) > remaining_points:
-            # not enough space for the whole sq, split it
+        if (end - start + 1) > remaining_points:
+            # Not enough space for the whole sq, split it
             real_end = end
             end = remaining_points + start - 1
-            assert((end-start+1) <= remaining_points)
-            sn_intervals[sn] = (end+1, real_end)
+            assert((end - start + 1) <= remaining_points)
+            sn_intervals[sn] = (end + 1, real_end)
             sns.insert(0, sn)
         interval = "%s\t%s\t%s\t+\t%s\n" % (sn, start, end, sn)
-        remaining_points -= (end-start+1)
+        remaining_points -= (end - start + 1)
 
-            #write to the file
-        f.write(interval)
-            
+        interval_list_text += interval
+
         if remaining_points <= 0:
-            break        
-    f.close()
+            break
 
-    chunk_input_pdh = os.getcwd()
-    print "Chunk intervals collection saved as: %s" % (chunk_input_pdh)
-    return chunk_input_pdh
+    return interval_list_text
+
 
 def main():
-
-    #parse the arguments from command line
-    # path_to_dict = sys.argv[1]
-    # todir = sys.argv[2]
-
     parser = argparse.ArgumentParser(description='Create Interval List')
-    parser.add_argument("--path", help="path to dict file")
-    parser.add_argument("--output_dir", help="output directory for interval list")
-    args = vars(parser.parse_args())
+    parser.add_argument("--path", help="Path to dict file")
+    parser.add_argument("--output_dir", help="Output directory for interval list")
+    args = parser.parse_args()
 
-    path_to_dict = args['path']
-    todir = args['output_dir']
-    
-    skip_sq_sn_regex = '_decoy$'
-    # if 'skip_sq_sn_regex' in current_job['script_parameters']:
-    #     skip_sq_sn_regex = current_job['script_parameters']['skip_sq_sn_regex']
-    skip_sq_sn_r = re.compile(skip_sq_sn_regex)
-    
     # Create an interval_list file for each chunk based on the .dict in the reference collection
 
-    #change to working directory
-    os.chdir(todir)
+    basename = os.path.splitext(os.path.basename(args.path))[0] + '.interval_list'
 
-    #open dict file
-    ref_input_pdh = open(path_to_dict, 'r')
+    with open(args.path, 'r') as ref_input_pdh:
+        interval_list = dict_to_interval_list(ref_input_pdh)
     
-    output_locator = create_interval_lists(ref_input_pdh, skip_sq_sn_r)
-
-    # Done!
+    with open(os.path.join(args.output_dir, basename), "w+") as out_file:
+        out_file.write(interval_list)
 
 
 if __name__ == '__main__':
