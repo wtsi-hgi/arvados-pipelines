3844c2c3159a3843c52622f895fc672d76d2f584
Mercury <mercury@sanger.ac.uk>
2017-11-08 22:49:42 +0000

    attempt to build 3.8 using openjdk

--- docker/gatk/build.sh
+++ docker/gatk/build.sh
@@ -1,11 +1,15 @@
-VERSIONS="3.4
-3.5
-3.6
-3.7
-3.8"
+#!/bin/bash
+
+SOJDK_VERSIONS=(3.4 3.5 3.6 3.7)
+OPENJDK_VERSIONS=(3.8)
 
 
-for version in $VERSIONS
+for version in ${SOJDK_VERSIONS[@]}
 do
-    docker build . -t gatk:$version --build-arg version=$version
+    docker build . -f Dockerfile-sojdk -t gatk:$version --build-arg version=$version
+done
+
+for version in ${OPENJDK_VERSIONS[@]}
+do
+    docker build . -f Dockerfile-openjdk -t gatk:$version --build-arg version=$version
 done
