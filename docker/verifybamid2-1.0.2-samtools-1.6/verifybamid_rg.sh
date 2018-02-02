896ea4b8cac19ef993c3bdca4ba169db4d3ac9ff
Joshua Randall <joshua.randall@sanger.ac.uk>
2018-02-02 17:00:43 +0000

    add verifybamid_rg wrapper script

---
+++ docker/verifybamid2-1.0.2-samtools-1.6/verifybamid_rg.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+# Process command line args
+TEMP=$(getopt -o c:r:f:p: --long cram-file:,rg:,ref-file:,svd-prefix: \
+     -n 'verifybamid_rg' -- "$@")
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+
+while true ; do
+        case "$1" in
+                -c|--cram-file) cram_file=$2 ; shift 2 ;;
+                -r|--rg) rg=$2 ; shift 2 ;;
+                -f|--ref-file) ref_file=$2 ; shift 2 ;;
+                -p|--svd-prefix) svd_prefix=$2 ; shift 2 ;;
+                --) shift ; break ;;
+                *) echo "Unrecognised option: $1" ; exit 1 ;;
+        esac
+done
+if [[ -n "${1+x}" ]]; then
+    echo "Unexpected arguments remaining after option processing: ${1}"
+    exit 1
+fi
+
+if [[ -z "${cram_file+x}" ]]; then
+    echo "--cram-file must be specified"
+    exit 1
+fi
+
+if [[ -z "${rg+x}" ]]; then
+    echo "--rg must be specified"
+    exit 1
+fi
+
+if [[ -z "${ref_file+x}" ]]; then
+    echo "--ref-file must be specified"
+    exit 1
+fi
+
+if [[ -z "${svd_prefix+x}" ]]; then
+    echo "--svd-prefix must be specified"
+    exit 1
+fi
+
+exclude_rgs=$(samtools view -H "${cram_file}" | awk 'BEGIN {FS="\t"; found=0; rg="'${rg}'";} $1=="@RG" {for(i=1; i<=NF; i++){if($i~/^ID:/){split($i,rg_id,":"); have_rg=rg_id[2]; if(have_rg==rg){found=1;} else {print have_rg;}}}} END {if(found==0){print "RG not found"; exit 1;} else {exit 0;}}')
+
+output_file="$(basename ${cram_file} .cram).verifybamid2"
+
+bed_file="${svd_prefix}.bed"
+ud_file="${svd_prefix}.UD"
+mu_file="${svd_prefix}.mu"
+
+VerifyBamID --PileupFile <(samtools mpileup -G <(echo "${exclude_rgs}") -f "${ref_file}" -l "${bed_file}" "${cram_file}") --UDPath "${ud_file}" --BedPath "${bed_file}" --MeanPath "${mu_file}" --Reference "${ref_file}" --Output "${output_file}"
+
