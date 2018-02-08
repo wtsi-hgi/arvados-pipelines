3111becd1beff50765b107a8056e1f62453ea191
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-02-08 00:52:35 +0000

    fix parsing of verifybamid2 output into read group caps file

--- docker/get-read-group-caps/get_read_group_caps.py
+++ docker/get-read-group-caps/get_read_group_caps.py
@@ -8,7 +8,9 @@ args = parser.parse_args()
 with open("caps_file", "w") as caps_file:
     for read_group, verify_bam_id_file_name in zip(args.read_groups, args.verify_bam_id_files):
         with open(verify_bam_id_file_name, "r") as verify_bam_id_file:
-            file_text = verify_bam_id_file.read()
-            alpha = file_text[file_text.find("Alpha:") + len("Alpha:"):]
-
-        caps_file.write(read_group + "\t" + alpha + " \n")
+            for line in verify_bam_id_file:
+                key, value = line.strip().split(':')
+                if key == "Alpha":
+                    alpha = value
+                    break
+        caps_file.write(read_group + "\t" + alpha + "\n")
