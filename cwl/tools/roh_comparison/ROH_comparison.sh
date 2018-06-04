7f64a07e58d50a96c73a384beb56f48e7cd11097
Sarah Chacko <sc35@sanger.ac.uk>
2018-06-04 17:13:43 +0100

    change region of homozygosity code due to changed truth set files

--- cwl/tools/roh_comparison/ROH_comparison.sh
+++ cwl/tools/roh_comparison/ROH_comparison.sh
@@ -4,61 +4,91 @@ set -u
 set -o pipefail
 
 # Input:
-#   a 'truth set' of ROH regions for a sample (already known) 
+#   a 'truth set' of ROH regions in samples for a chromosome (already known) 
 #   a combined vcf including the same sample.
 
 # Output:
 #   a measure of the 'correctness' of the vcfs in those regions, based on the 
 #   number of heterozygous calls 
 #   in regions known to be homozygous, compared to all heterozygous calls
-#   This is called from an analysis workflow to process a set of sample bed files in parallel 
+#   This is called from an analysis workflow to process a set of truth ROH files in parallel 
 #   against the same vcf file
 #   Original use: vareval project (to compare variant callers)
 
 # Checks that should have been done before calling this:    
-# The sample names match in both files. 
+# The sample names match in both files. (In the original case they are prefixed with sc_autozyg
+# in the truth set but not in the vcf)
 # Both files use the same reference genome (and chromosome names match)
 
 # vcf is standard format, multisample
-# truthset format is bed: [1]Chromosome	[2]Start [3]End	[4]name
+# truthset format is format RG	[2]Sample	[3]Chromosome	[4]Start	[5]End	
+# [6]Length (bp)	[7]Number of markers	[8]Quality (average fwd-bwd phred score)
 
-# commandline './ROH_comparison <truthDir> <experimentalvcf>'
+# commandline './ROH_comparison <truthDir> <experimentalvcf> <samplemappings>'
 truthROH=$1
 expVCF=$2
+sampleMap=$3
 
-# Truthset files are in one directory and named by sample, as <sample>_roh_hg38.bed
- 
+# Truthset files are in one directory and named by chromosome, eg chr1.ROHs.txt 
 # There are bcftools view options to take out specific samples -S sample file
+chromosome=$(basename $truthROH)
+echo $chromosome
 
 #echo -e "sample"'\t'"ROHHets"'\t'"AllHets"
 #echo $f
-sample=$(basename $truthROH | xargs |  cut -d '_' -f 1) # filename and trim whitespace
+#sample=$(basename $truthROH | xargs |  cut -d '_' -f 1) # filename and trim whitespace
+# get samples 
+bcftools query -l $expVCF > temp_samples.txt
 #echo $sample
+while read sample; do
+    echo $sample
+    # is the sample in the chromosome truth set?
+    echo "Here" 
+    s=$(grep "$sample" "$sampleMap")
+    echo $s
+    # the second value which starts with sc_ is the one we want
+    for word in $s 
+    do
+        real_sample=$word
+    done
+    echo $real_sample
+    a=$(grep -c "$real_sample" "$truthROH")
+    echo "count is" $a
+    if [ $a = 0 ]
+    then
+     echo $sample not found
+    fi
+    echo "x"
+    # Make a bed file for that sample truth set chrom start end
+    grep $real_sample $truthROH | cut -f3-5  > $sample.bed
+    # get single sample vcf#
+    echo "xx"
+    bcftools view -s $sample $expVCF  | \
+    # apply filters if set   
+    bcftools view -f PASS,. | \
+    # take only heterozygous calls
+    bcftools view -g het  > hets_"$sample"_"$chromosome".vcf
+    # compress
+     echo "xxx"
+    bcftools view -Oz -o hets_"$sample"_"$chromosome".vcf.gz hets_"$sample"_"$chromosome".vcf
+    # index
+    bcftools index hets_"$sample"_"$chromosome".vcf.gz
+    
+    # find het calls in ROH regions
+    echo "xxxx"
+    bcftools view -R "$sample".bed hets_"$sample"_"$chromosome".vcf.gz > hets_in_ROH_"$sample"_"$chromosome".vcf 
+    h="$(bcftools view -H hets_in_ROH_"$sample"_"$chromosome".vcf | wc -l)"
+    g="$(bcftools view -H hets_"$sample"_"$chromosome".vcf | wc -l)"
+    
+    #output
+    echo -e "$chromosome"'\t'"$expVCF"'\t'"$sample"'\t'"$h"'\t'"$g"
 
-# get single sample vcf
-bcftools view -s $sample $expVCF  | \
-# apply filters if set
-bcftools view -f PASS,. | \
-# take only heterozygous calls
-bcftools view -g het  > "hets_$sample.vcf"
-# compress
-bcftools view -Oz -o "hets_$sample.vcf.gz" "hets_$sample.vcf"
-# index
-bcftools index "hets_$sample.vcf.gz"
-  
-# find het calls in ROH regions
-bcftools view -R $truthROH "hets_$sample.vcf.gz" > "hets_in_ROH_$sample.vcf" 
-h="$(bcftools view -H hets_in_ROH_$sample.vcf | wc -l)"
-g="$(bcftools view -H hets_$sample.vcf | wc -l)"
-  
-#output
-echo -e "$expVCF"'\t'"$sample"'\t'"$h"'\t'"$g"
-
+done < temp_samples.txt
 #cleanup (left from when it was a loop, probably not needed)
-rm hets_$sample.vcf
-rm hets_$sample.vcf.gz
-rm hets_in_ROH_$sample.vcf 
-rm *.csi   
+#rm hets_$sample.vcf
+#rm hets_$sample.vcf.gz
+#rm hets_in_ROH_$sample.vcf 
+#rm *.csi   
 
 
 
--- cwl/workflows/analysis-workflows/ROH_workflow.cwl
+++ cwl/workflows/analysis-workflows/ROH_workflow.cwl
@@ -1,9 +1,10 @@
 cwlVersion: v1.0
 class: Workflow
 
-# input the script, the array of sample files and a multisample vcf and scatter 
+# input the script, the array of ROH files and a multisample vcf and scatter 
 # the ROH calculations then combine the results (vcf, sample, hets in ROH, all hets, 
 # tab separated) to a single output file
+# Before running on Arvados the files needed must be in keep, and a yaml with the file array generated.
 
 requirements:
   - class: ScatterFeatureRequirement
@@ -12,25 +13,28 @@ requirements:
 inputs:
   - id: script
     type: File
-  - id: ROH_beds
+  - id: ROH_chr
     type: File[]
   - id: vcf_file
     type: File
+  - id: sample_mapping
+    type: File    
     
 steps:
 
   - id: ROH_calc
     scatter:
       - filein_ROH      
-    run: ROH_comparison.cwl
+    run: ../../tools/roh_comparison/ROH_comparison.cwl
     in:
       script: script
-      filein_ROH: ROH_beds
-      filein_VCF: vcf_file          
+      filein_ROH: ROH_chr
+      filein_VCF: vcf_file 
+      sample_mapping: sample_mapping       
     out: [output]
 
   - id: ROH_combine
-    run: ROH_combine.cwl
+    run: ../../tools/roh_comparison/ROH_combine.cwl
     in:
       files:  ROH_calc/output
     out: [stats] 
